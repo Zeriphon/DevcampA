e33c4e09b24cb97f1294de70741781aef848a28c
Alpine Devcamp <alpine@devcamp.com>
2017-06-21 15:51:49 +0000

    :

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,2 +1,8 @@
 class Skill < ApplicationRecord
+    include Placeholder
+    after_initialize :set_defaults
+    
+    def set_defaults
+        self.badge ||= Placeholder.image_generator(height: '600', width: '400')
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,16 @@
+3.times do |topic|
+    Topic.create!(
+        title: "Topic #{topic}"
+        )
+end
+
+
 10.times do |blog|
-    Blog.create!(
-        title: "BP #{blog}",
-        body: "adido"
-    ) 
+Blog.create!(
+    title: "BP #{blog}",
+    body: "adido",
+    topic_id: Topic.last.id
+) 
 end
 
 puts "10 blog posts created"
@@ -27,3 +35,10 @@ puts "5 skills created"
 end
 
 puts "9 portfolio items created"
+
+3.times do |technology|
+    Technology.create!(
+            name: "Technology #{technology}",
+            portfolio_id: Portfolio.last.id
+        )
+    end
