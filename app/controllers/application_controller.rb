7e705b957db208a7403e3cd0ef9019456307788a
Alpine Devcamp <alpine@devcamp.com>
2017-06-28 15:05:33 +0000

    :

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::Base
   
   before_action :configure_permitted_parameters, if: :devise_controller?
   
+  def set_source
+    session[:source] = params[:q] if params[:q]
+  end
+  
   def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
       devise_parameter_sanitizer.permit(:account_update, keys: [:name])
