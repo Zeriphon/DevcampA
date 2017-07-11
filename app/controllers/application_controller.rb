00eb893d6953359267a1f1a42327486236d67007
Alpine Devcamp <alpine@devcamp.com>
2017-07-11 15:07:29 +0000

    :

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  include ApplicationHelper
   
   before_action :configure_permitted_parameters, if: :devise_controller?
   
@@ -18,16 +19,4 @@ class ApplicationController < ActionController::Base
     @page_title = "Devcamp Portfolio | My Portfolio Website"
   end
   
-  def set_copyright
-    @copyright = AciViewTool::Renderer.copyright 'Aciojn Quazson', 'All rights yet to be reserved.'
-  end
-end
-
-
-module AciViewTool
-  class Renderer
-    def self.copyright name, msg
-      "&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
-    end
-  end
 end
